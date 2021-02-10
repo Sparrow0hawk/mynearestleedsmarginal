63a0f03dc1919dc8575461436e8e77f494699dee
sparrow0hawk <alexjcoleman@gmail.com>
2021-02-10 21:07:42 +0000

    adjusted leafletmap height to 75vh

--- ui.R
+++ ui.R
@@ -79,7 +79,7 @@ ui <- fluidPage(
       ),
                   #textOutput("value2")),
       tags$br(),
-      leafletOutput("mymap", height = "95vh"),
+      leafletOutput("mymap", height = "75vh"),
       tags$p(' ')
       ,tags$p(align = 'center',
               tags$a(class = 'Linkbutton2',
