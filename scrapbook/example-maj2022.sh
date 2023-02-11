f28bca991bf329228c7cbcb09298e4db62ae53fb
Sparrow0hawk <alexjcoleman@gmail.com>
2023-02-11 12:00:58 +0000

    update results scraping script

--- scrapbook/example-maj2022.sh
+++ scrapbook/example-maj2022.sh
@@ -3,17 +3,17 @@
 DATA_PATH="assets/data"
 
 # download data file
-R -e "download.file('https://datamillnorth.org/download/election-results-local/773088bc-22fd-496b-9362-82235b230ced/Results_06May2021.CSV',destfile='assets/data/data2021.csv')"
+R -e "download.file('https://datamillnorth.org/download/election-results-local/1c4031c8-baa6-41f7-b46c-b7c368c0678f/Results_05May2022.CSV',destfile='assets/data/data2022.csv')"
 
 # rename columns
 
-Rscript scrapbook/rename_cols.R $DATA_PATH/data2021.csv $DATA_PATH/data-new2021.csv CandidateDescription AreaName
+Rscript scrapbook/rename_cols.R $DATA_PATH/data2022.csv $DATA_PATH/data-new2022.csv CandidateDescription AreaName
 
 # calculate majorities
-Rscript scrapbook/calculating_majorities.R $DATA_PATH/data-new2021.csv $DATA_PATH/maj2022.csv
+Rscript scrapbook/calculating_majorities.R $DATA_PATH/data-new2022.csv $DATA_PATH/maj2023.csv
 
 # create geojson file
-Rscript scrapbook/singleDF.R $DATA_PATH/maj2022.csv main_file FALSE
+Rscript scrapbook/singleDF.R $DATA_PATH/maj2023.csv main_file FALSE
 
 # tidy up temporary files
-rm $DATA_PATH/data2021.csv $DATA_PATH/data-new2021.csv $DATA_PATH/maj2022.csv
+rm $DATA_PATH/data2022.csv $DATA_PATH/data-new2022.csv $DATA_PATH/maj2023.csv
