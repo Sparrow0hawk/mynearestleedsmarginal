b9d2d7efabc45bc02f168efeb30f27a57d564757
Sparrow0hawk <alexjcoleman@gmail.com>
2023-02-14 08:32:43 +0000

    correct deploy script and use ver rather than date

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-DATE=$(date +"%Y-%m-%d")
+VER="2023-01"
 
 PROJECT=$(gcloud config get-value project)
 
-docker build . -f dockerfiles/Dockerfile -t mynearestleedsmarg:$DATE
+docker build . -f dockerfiles/Dockerfile -t mynearestleedsmarg:$VER
 
-docker tag mynearestleedsmarg:$DATE gcr.io/$PROJECT/mynearestleedsmarg:$DATE
+docker tag mynearestleedsmarg:$VER gcr.io/$PROJECT/mynearestleedsmarg:$VER
 
-docker push gcr.io/$PROJECT/mynearestleedsmarg:$DATE
+docker push gcr.io/$PROJECT/mynearestleedsmarg:$VER
 
 gcloud run deploy mynearestleedsmarg-deploy \
-                  --image gcr.io/leeds-app-2-serverless/mynearestleedsmarg:$DATE \
-                  --region "europe-west1"
+                  --image gcr.io/leeds-app-2-serverless/mynearestleedsmarg:$VER \
+                  --region "europe-west1" \
                   --platform managed \
-                  --port=3838 \
+                  --port=3838 
 
  
