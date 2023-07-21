7baabf71f2f9e3650a21f0dace1ec78db27010eb
Sparrow0hawk <alexjcoleman@gmail.com>
2023-07-21 20:23:52 +0100

    update terraform setup script

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -1,11 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+PROJECT=mynearestleedsmarginal-prod
 
 # create project in gcp
-gcloud projects create test-cloud-run-iap --name="Test terra shiny"
+#gcloud projects create $PROJECT --name="mynearestleedsmarg web app"
 
 # set current project with gcloud as this project
-gcloud config set project test-cloud-run-iap
+#gcloud config set project $PROJECT
 
-gcloud beta billing projects link test-cloud-run-iap \
+gcloud beta billing projects link $PROJECT \
  --billing-account $BILLING_ACCOUNT_ID
 
 gcloud services enable \
@@ -13,12 +18,3 @@ gcloud services enable \
     iam.googleapis.com \
     artifactregistry.googleapis.com
 
-gcloud iam service-accounts create terrashiny \
- --display-name "Terra shiny service account"
-
-gcloud projects add-iam-policy-binding test-cloud-run-iap \
-    --member="serviceAccount:terrashiny@test-cloud-run-iap.iam.gserviceaccount.com" \
-    --role="roles/owner"
-
-gcloud iam service-accounts keys create .credentials.json \
-    --iam-account=terrashiny@test-cloud-run-iap.iam.gserviceaccount.com
