ce4b3f10f4bad6f22256f3896684d7a6c7765111
Sparrow0hawk <alexjcoleman@gmail.com>
2023-07-21 20:23:40 +0100

    update tf outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,16 @@
 output "app-url" {
-  value = google_cloud_run_v2_service.default.uri
+  value = google_cloud_run_service.default.status[0].url
+}
+
+output "project" {
+  value = var.project
+}
+
+output "service-account-key" {
+  value     = google_service_account_key.mykey.private_key
+  sensitive = true
+}
+
+output "service-account-email" {
+  value = google_service_account.cloud-run-service-act.email
 }
